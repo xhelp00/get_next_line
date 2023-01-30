d278af6b42c9e3e02a60bb9024f2dcf61cc4fff0
Petr Helebrant <phelebra@c3r8s4.42prague.com>
2023-01-30 17:17:43 +0100

    prepared util functions and header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,14 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:52:52 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/30 09:56:16 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/30 11:08:03 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,57 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:52:36 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/30 09:56:40 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/30 11:08:09 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*join;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	join = malloc (sizeof(char) * (len1 + len2) + 1);
+	if (!join)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+		join[i++] = s2[j++];
+	join[i] = '\0';
+	return (join);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (!*s)
+			return (0);
+		s++;
+	}
+	return ((char *)s);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
