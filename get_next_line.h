58b869650577f119cf7f5ad1d3f33a5f9421fd59
Petr Helebrant <phelebra@c3r8s3.42prague.com>
2023-02-03 08:13:59 +0100

    fixrepo

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,24 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:52:52 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/30 11:08:03 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/02/02 17:24:51 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4242
+# endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 
 
+#endif
